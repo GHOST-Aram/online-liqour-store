ebb357eb495fd9d75e6cd1ca0b3e5a554bef71b2
GHOST-Aram <owinoojala@gmail.com>
2023-06-10 10:49:36 +0300

    Feat: Create Header, Navigation and Logo components
         -Implement Routes and display Logo

--- src/apps/App.jsx
+++ src/apps/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import '../styles/index.css'
+import { BrowserRouter as Router } from 'react-router-dom';
+import RouteSwitch from './RouteSwitch';
+import Header from '../components/Header';
 function App() {
   return (
     <div className="App">
-    <h1>hello kenya</h1>     
+		<Header />
+        <Router><RouteSwitch /></Router>
     </div>
   );
 }
---
+++ src/apps/RouteSwitch.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './Home'
+import Shop from './Shop'
+import Cart from './Cart'
+
+const RouteSwitch = () => {
+  return (
+    <Routes >
+        <Route path='/' element = {<Home />} />
+        <Route path='/shop' element = {<Shop />} />
+        <Route path='/cart' element = {<Cart />} />
+    </Routes>
+  )
+}
+
+export default RouteSwitch
