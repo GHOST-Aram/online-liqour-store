5a8da93176f0c7cee7255bf6f199ec379b260d11
GHOST-Aram <owinoojala@gmail.com>
2023-06-10 08:19:53 +0300

    Chore: COnfigure Tailwind and generate css normalizer

--- src/apps/App.jsx
+++ src/apps/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 function App() {
   return (
     <div className="App">
-    Live      
+    <h1>hello kenya</h1>     
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css';
+import './styles/normalize.css';
 import App from './apps/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
