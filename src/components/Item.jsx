1730f3cfa5a29d169d0f5fd322b17e94533f96ed
GHOST-Aram <owinoojala@gmail.com>
2023-06-11 10:12:38 +0300

    feat: Style and display shelved items on home page

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,22 +2,32 @@ import React from 'react'
 import Button from './Button'
 import Heading from './Heading'
 
+const calculateDiscount = (previousPrice, currentPrice) =>{
+    return Math.floor(((currentPrice - previousPrice) / previousPrice) * 100)
+}
 
 const Item = (props) => {
     const {currentPrice,previousPrice, productImage, productName} = props
-    const productDiscount = ((currentPrice - previousPrice) / previousPrice) * 100
+    const productDiscount = calculateDiscount(previousPrice, currentPrice)
 
     return (
-        <div>
+        <div className='item p-2 rounded-md'>
             <img src={productImage} alt={productName} />
-            <h2 aria-label='product name'>{productName}</h2>
-            <div>
-                <h1 aria-label='current price'>Ksh. {currentPrice}</h1>
-                <span aria-label='previous price'>{previousPrice}</span>
-                <span aria-label='discount'>{productDiscount}%</span>
+            <h2 aria-label='product name'
+                className='font-bold text-red-700 text-start'
+            >{productName}</h2>
+            <div className='flex flex-row gap-4 my-2'>
+                <h1 aria-label='current price'
+                    className='font-bold'
+                >Ksh. {currentPrice}</h1>
+                <p aria-label='previous price'
+                    className='text-crossed text-slate-500'
+                >{previousPrice}</p>
+                <span aria-label='discount'
+                    className='text-red-400'
+                >{productDiscount}%</span>
             </div>
-            <Button>Add to Cart</Button>
-
+            <Button className={'w-full'}>Add to Cart</Button>
         </div>
     )
 }
