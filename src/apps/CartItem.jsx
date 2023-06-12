077285bb60ed5ba28a312e1a17dd91d0778cd6eb
GHOST-Aram <owinoojala@gmail.com>
2023-06-12 11:29:23 +0300

    feat: Creat and test CartItem component
         -Refactor CartItem tests to accept one object prop

--- src/apps/CartItem.jsx
+++ src/apps/CartItem.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
-const CartItem = () => {
-  return (
-    <div>CartItem</div>
-  )
+const CartItem = ({item}) => {
+	return (
+		<div aria-label='cart item'>
+			<div>
+				<img src={item.image} alt={item.name} />
+				<h1 aria-label='name'>{item.name}</h1>
+			</div>
+			<div>
+				<h2 aria-label='price'>{item.price}</h2>
+				<div>
+					<button aria-label='decrement quantity'>-</button>
+					<h2 aria-label='quantity'>{item.quantity}</h2>
+					<button aria-label='increment quantity'>+</button>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default CartItem
--- src/tests/CartItem.test.jsx
+++ src/tests/CartItem.test.jsx
@@ -1,67 +1,104 @@
 import { render, screen } from "@testing-library/react";
 import CartItem from "../apps/CartItem";
+import red_label from '../images/drinks/jonnie_wlaker_red_label.png'
+
+const item = { 
+    name: 'Red Label', price: 2000, 
+    quantity: 1, image: red_label,
+}
 
 test('Renders aria-label for cart item', () =>{
-    render(<CartItem />)
+    render(<CartItem item={item}/>)
     
     const cartItemNode = screen.getByLabelText('cart item')
     expect(cartItemNode).toBeInTheDocument()
 })
 
 test('Renders cart Item image', () =>{
-    render(<CartItem />)
+    render(<CartItem item={item}/>)
 
     const cartItemImage = screen.getByRole('img')
     expect(cartItemImage).toBeInTheDocument()
 })
 
-test('Renders cart Item alt text', () =>{
-    render(<CartItem itemName = {'Hunter\'s Choice'} />)
+test('Renders item name', () =>{
+    render(<CartItem item={item}/>)
 
-    const altText = screen.getByAltText(/Hunter's Choice/)
+    const name = screen.getByText(/Red Label/)
+    expect(name).toBeInTheDocument()
+})
+
+test('Renders aria -label for item name', () =>{
+    render(<CartItem item={item}/>)
+
+    const nameNode = screen.getByLabelText('name')
+    expect(nameNode).toBeInTheDocument()
+})
+
+test('Renders cart Item alt text', () =>{
+    render(<CartItem item = {item} />)
+
+    const altText = screen.getByAltText(/Red Label/)
     expect(altText).toBeInTheDocument()
 })
 
 test('Renders arial-label for item price', () =>{
-    render(<CartItem />)
+    render(<CartItem item={item}/>)
 
-    const priceNode = screen.getByLabelText('current price')
+    const priceNode = screen.getByLabelText('price')
     expect(priceNode).toBeInTheDocument()
 })
 test('Renders cart item current price', () =>{
-    render(<CartItem itemPrice = {1230} />)
+    render(<CartItem item = {item} />)
 
-    const itemPrice = screen.getByText(/1230/)
+    const itemPrice = screen.getByText(/2000/)
     expect(itemPrice).toBeInTheDocument()
 })
 
 test('Renders aria-label for increment button', () =>{
-    render(<CartItem />)
+    render(<CartItem item={item}/>)
 
     const incrementBtn = screen.getByLabelText('increment quantity')
     expect(incrementBtn).toBeInTheDocument()
 })
 
-test('Renders increment button', () =>{
-    render(<CartItem />)
+test('Renders aria-label of decrement button', () =>{
+    render(<CartItem item={item}/>)
 
-    const incrementBtn = screen.getByRole('button')
-    const incrementBtnText = screen.getByText('+')
-
+    const incrementBtn = screen.getByLabelText('decrement quantity')
     expect(incrementBtn).toBeInTheDocument()
+})
+test('Renders increment button', () =>{
+    render(<CartItem item={item}/>)
+
+    const incrementBtnText = screen.getByText(/\+/)
     expect(incrementBtnText).toBeInTheDocument()
 })
 
+test('Renders decrement button', () =>{
+    render(<CartItem item={item}/>)
+
+    const incrementBtnText = screen.getByText(/-/)
+    expect(incrementBtnText).toBeInTheDocument()
+})
+
+test('Renders two buttons increment and decrement', () =>{
+    render(<CartItem item={item} />)
+
+    const buttons = screen.getAllByRole('button')
+    expect(buttons.length).toEqual(2)
+})
+
 test('Renders item quantity', () =>{
-    render(<CartItem quantity = {1}/>)
+    render(<CartItem item = {item}/>)
 
     const quantityText = screen.getByText(/1/)
     expect(quantityText).toBeInTheDocument()
 })
 
 test('Renders arial-label for item quantity', () =>{
-    render(<CartItem />)
+    render(<CartItem item={item}/>)
 
-    const quantityNode = screen.getByLabelText(/quantity/)
+    const quantityNode = screen.getByLabelText('quantity')
     expect(quantityNode).toBeInTheDocument()
 })
