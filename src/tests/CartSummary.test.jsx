b4cc12ae790f4b7f589a8099710a0ecdef880aec
GHOST-Aram <owinoojala@gmail.com>
2023-06-14 19:05:08 +0300

    chore: Refactor CartSummary tests
         -Rewrite tests using DocumentTester Class
         -Remove Repeated tests for Button and Heading

--- src/tests/CartSummary.test.jsx
+++ src/tests/CartSummary.test.jsx
@@ -1,38 +1,20 @@
-import { render, screen } from "@testing-library/react";
 import CartSummary from "../components/CartSummary";
+import { DocumentTester } from "../utils/DocumentTester";
 
+const tester = new DocumentTester()
 
-test('Renders cart summary heading', () =>{
-    render(<CartSummary />)
+tester.testByText(
+    {
+        message: 'Renders number of items in the cart',
+        component: <CartSummary totalQuantity={19}/>,
+        text: /19/
+    }
+)
 
-    const heading = screen.getByText(/Cart summary/i)
-    expect(heading).toBeInTheDocument()
-})
-
-test('Renders number of items in the cart', () =>{
-    render(<CartSummary totalQuantity = {19}/>)
-
-    const numberOfitems = screen.getByText(/19/)
-    expect(numberOfitems).toBeInTheDocument()
-})
-
-test('Renders order total', () =>{
-    render(<CartSummary orderTotal = {2300} />)
-
-    const orderTotal = screen.getByText(/2300/)
-    expect(orderTotal).toBeInTheDocument()
-})
-
-test('Renders chackout button', () =>{
-    render(<CartSummary />)
-
-    const checkoutBtn = screen.getByRole('button')
-    expect(checkoutBtn).toBeInTheDocument()
-})
-
-test('Renders checkout button text', ()=>{
-    render(<CartSummary />)
-    
-    const buttonText = screen.getByText(/Checkout/i)
-    expect(buttonText).toBeInTheDocument()
-})
+tester.testByText(
+    {
+        message: 'Renders order total',
+        component: <CartSummary orderTotal={2300} />,
+        text: /2300/
+    }
+)
