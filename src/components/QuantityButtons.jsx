77b85179ff36a959ef70762dd5116020f1ab8dcd
GHOST-Aram <owinoojala@gmail.com>
2023-06-14 03:54:03 +0300

    fix: Fix bug in updating quantity of items in cart
        -use the new refactored Cart.js utility class

--- src/components/QuantityButtons.jsx
+++ src/components/QuantityButtons.jsx
@@ -1,36 +1,22 @@
-import React, { useState } from 'react'
+import React from 'react'
 import {FaMinus, FaPlus} from 'react-icons/fa'
 import { CartHandler } from '../utils/Cart'
 
 const QuantityButtons = ({item, orders, setOrders}) => {
-    const [quantity, setQuantity] = useState(item.quantity)
     const cart = new CartHandler(orders)
 
-    const decrementQuantity = (item) =>{
-        setQuantity(()=>{
-            if(quantity > 1)
-                return quantity - 1
-            return 1
-        })
-        setOrders(cart.decrementItemQuantity(item))
-    }
     
-    const incrementQuantity  = (item) =>{
-        setQuantity(()=>quantity + 1)
-        setOrders(cart.incrementItemQuantity(item))
-        
-    }
     return (
         <div className='flex flex-row gap-2 items-center '>
             <button  aria-label='decrement quantity'
-            onClick={()=>decrementQuantity(item)}
+            onClick={()=>setOrders(cart.decrementItemQuantity(item.id))}
                 className={'bg-red-700 py-2 px-4 text-slate-300'+
                 'decrement-btn rounded-md font-bold'}
             ><FaMinus/>
             </button>
-            <h2 aria-label='quantity' className='font-bold'>{quantity}</h2>
+            <h2 aria-label='quantity' className='font-bold'>{item.quantity}</h2>
             <button aria-label='increment quantity'
-            onClick={() => incrementQuantity(item)}
+            onClick={() => setOrders(cart.incrementItemQuantity(item.id))}
                 className= {'bg-red-700 py-2 px-4 text-slate-300 rounded-md font-bold'}
             ><FaPlus/>
             </button>
