3e357e981f108ef4a498280fa9f84caa0f4e42b4
GHOST-Aram <owinoojala@gmail.com>
2023-06-15 06:45:51 +0300

    chore: Merge tests into components

--- src/tests/Logo.test.jsx
+++
@@ -1,9 +0,0 @@
-import {render, screen} from '@testing-library/react'
-import Logo from '../components/Logo'
-
-test('Renders Site Logo', () =>{
-    render(<Logo/>)
-
-    const logo = screen.getByAltText('Valhala')
-    expect(logo).toBeInTheDocument()
-})
