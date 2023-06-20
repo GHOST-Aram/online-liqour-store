54cff3ec0af6bc49f630dc2ea8e04f4887b5a285
GHOST-Aram <owinoojala@gmail.com>
2023-06-20 19:33:18 +0300

    refactor: Refactor Showcase tests using DomTester

--- src/components/Showcase.test.jsx
+++ src/components/Showcase.test.jsx
@@ -1,36 +1,52 @@
-import { render, screen } from "@testing-library/react";
+import { DocumentTester } from "../utils/DocumentTester";
 import Showcase from "../components/Showcase";
 import { BrowserRouter as Router } from "react-router-dom";
 
-test('Renders showcase text', ()=>{
-    render(<Showcase/>, {wrapper: Router})
+const inDOmTester = new DocumentTester()
 
-    const showcaseText = screen.getByText(
-        /We deliver your favorite whisky and wine to your door step/i)
-    expect(showcaseText).toBeInTheDocument()
-})
-test('Renders showcase images', () =>{
-    render(<Showcase/>, {wrapper: Router})
-    
-    const canadianHunters = screen.getByAltText(/Canadian Hunters/i)
-    expect(canadianHunters).toBeInTheDocument()
-})
+inDOmTester.testByText(
+    {
+        message: 'Renders main heading in hero section',
+        component: <Showcase />,
+        wrapper: Router,
+        text: /We deliver your favorite whisky and wine to your door step/
+    }
+)
 
-test('Renders showcase order button', () =>{
-    render(<Showcase/>, {wrapper: Router})
-    
-    const orderbtn = screen.getByText(/Order Now/)
-    expect(orderbtn).toBeInTheDocument()
-})
 
-test('Renders drinking warning text', ()=>{
-    render(<Showcase/>, {wrapper: Router})
+inDOmTester.testByAltText(
+    {
+        message: 'Renders hero image',
+        component: <Showcase />,
+        wrapper: Router,
+        altText: /Canadian Hunters/
+    }
+)
 
-    const warningtext = screen.getByText(/Excessive consumption of alcohol is harmfull to your health/i)
-    const warningtext1 = screen.getByText(/Drink Responsibly/)
-    const warningtext2 = screen.getByText(/Not For sale to Persons under the age of 21 yrs/i)
 
-    expect(warningtext).toBeInTheDocument()
-    expect(warningtext1).toBeInTheDocument()
-    expect(warningtext2).toBeInTheDocument()
-})
+inDOmTester.testByText(
+    {
+        message: 'Renders first warning text',
+        component: <Showcase />,
+        wrapper: Router,
+        text: /Excessive consumption of alcohol is harmfull to your health/
+    }
+)
+
+inDOmTester.testByText(
+    {
+        message: 'Renders second warning text',
+        component: <Showcase />,
+        wrapper: Router,
+        text: /Drink Responsibly/
+    }
+)
+
+inDOmTester.testByText(
+    {
+        message: 'Renders last warning text',
+        component: <Showcase />,
+        wrapper: Router,
+        text: /Not for sale to persons under the age of 21 yrs/i
+    }
+)
